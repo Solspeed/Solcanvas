d58ce6ae8f327481bd3ffccbacab615622e8671c
codecShivam <codecshivam@gmail.com>
2024-02-13 00:36:56 +0530

    improved nav responsiveness

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,7 +2,7 @@ import Navbar from "../components/layout/Navbar";
 
 export default function Home() {
   return (
-    <main className="bg-black flex min-h-screen flex-col items-center justify-between p-6 lg:p-24">
+    <main className="bg-black flex min-h-screen min-w-screen flex-col items-center justify-between p-6 lg:p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <Navbar />
       </div>
