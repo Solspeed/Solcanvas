6b3e1e9b39c689559af5e3959c57bd4de4c307d9
codecShivam <codecshivam@gmail.com>
2024-02-13 21:18:48 +0530

    building build overview component

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,20 @@
 import Navbar from "../components/layout/Navbar";
+import Overview from "../components/layout/Overview";
 
 export default function Home() {
   return (
-    <main className="bg-black flex min-h-screen min-w-screen flex-col items-center justify-between p-6 lg:p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <Navbar />
+    <main>
+      <div className="w-full relative bg-black overflow-hidden flex flex-col items-center justify-start pt-[5.81rem] px-[0rem] pb-[0rem] box-border gap-[14.19rem] tracking-[normal] mq450:gap-[14.19rem] mq1000:gap-[14.19rem]">
+        <section className="w-[82.5rem] flex flex-row items-start justify-start py-[0rem] pr-[2.13rem] pl-[1.25rem] box-border max-w-full">
+          <div className="flex-1 flex flex-col items-center justify-start gap-[6.19rem] max-w-full mq725:gap-[6.19rem] mq450:gap-[6.19rem]">
+            <div className="w-[65.38rem] flex flex-row items-start justify-start pt-[0rem] pb-[2.38rem] pr-[0rem] pl-[0.94rem] box-border top-[0] z-[99] sticky max-w-full">
+              <Navbar />
+            </div>
+            <div className="w-[61.75rem] flex flex-row items-start justify-start py-[0rem] pr-[0rem] pl-[0.94rem] box-border max-w-full">
+              <Overview />
+            </div>
+          </div>
+        </section>
       </div>
     </main>
   );
---
+++ apps/web/components/layout/Overview.tsx
@@ -0,0 +1,24 @@
+export default function Overview() {
+    return (
+        <div className="flex-1 flex flex-col items-center text-white justify-start gap-[4.38rem] max-w-full text-center text-[5.4rem] font-inter mq725:gap-[4.38rem] mq1000:gap-[4.38rem] h-screen">
+            <h1 className="m-0 self-stretch  relative text-inherit  sm:tracking-[0.0em] leading-[1.88rem] capitalize font-bold font-inherit inline-block mq450:text-[1.6rem] mq450:leading-[0.75rem] mq1000:text-[3.4rem] mq1000:leading-[3.9rem]">
+                <p className="[margin-block-start:0] [margin-block-end:100px] mq1000:[margin-block-end:60px]">
+                    Explore projects
+                </p>
+                <p className="m-0">around ecosystem</p>
+            </h1>
+            <b className="relative text-[1.56rem] tracking-[0.03em] leading-[1.88rem] capitalize inline-block text-gray-400 max-w-full mq1000:text-[1.2rem] md:leading-[1.5rem]">
+                There are almost 400+ projects all around the Solana ecosystem.
+            </b>
+
+            <div className="flex flex-row items-start justify-start box-border max-w-full">
+                <button className="cursor-pointer [border:none] mq1000:py-[1.4rem] py-[1.6rem] px-12 mq1000:px-8 bg-white rounded-full flex flex-row items-center justify-center box-border max-w-full whitespace-nowrap hover:bg-gainsboro mq450:pl-[1.25rem] mq450:pr-[1.25rem] mq450:box-border">
+                    <b className="relative text-[1.88rem] mq1000:text-[1.6rem] tracking-[0.03em] leading-[1.88rem] capitalize font-inter text-[#867ad2] text-center z-[1]">
+                        Explore Projects
+                    </b>
+                </button>
+            </div>
+        </div>
+    );
+};
+
