794de4685db1d6feefbba8ac77760875339810a0
codecShivam <codecshivam@gmail.com>
2024-02-18 17:38:48 +0530

    ✨ Improve SEO: Add OG and Twitter image properties

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -7,6 +7,15 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "SolCanvas",
   description: "SolCanvas is a decentralized platform.",
+  openGraph: {
+    url: 'https://solcanvas.vercel.app/',
+    images: {
+      url: '../public/og.png',
+      alt: 'About Acme',
+      width: 1200,
+      height: 630,
+    },
+  },
 };
 
 export default function RootLayout({
