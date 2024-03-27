36a47a8eef41d410476cb7bb69c013792cb9d675
codecShivam <codecshivam@gmail.com>
2024-03-27 11:16:14 +0530

    building nav of marketplace

--- apps/web/app/marketplace/components/Navbar.tsx
+++ apps/web/app/marketplace/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 
 export default function Navbar() {
     return (
-        <div className="bg-gray-300 sm:py-[3.2rem] py-6 flex items-center justify-center">
+        <div className="bg-black sm:py-[3.2rem] py-6 flex items-center justify-center">
             <div className="min-w-[1169px] h-[50px] flex justify-between items-center">
                 <a href="" className="text-zinc-400 text-xl font-bold font-['Inter'] capitalize leading-[30px] tracking-tight">Digital Collectibles</a>
                 <a href="" className="text-zinc-400 text-xl font-bold font-['Inter'] capitalize leading-[30px] tracking-tight">Trading</a>
--- apps/web/app/marketplace/page.tsx
+++ apps/web/app/marketplace/page.tsx
@@ -1,9 +1,11 @@
+import ItemBar from "./components/ItemBar"
 import Navbar from "./components/Navbar"
 
 export default function page() {
     return (
-        <div className="w-screen bg-black">
-            <Navbar />
+        <div className="w-screen h-screen bg-black">
+            {/* <Navbar /> */}
+            <ItemBar />
         </div>
     )
 }
