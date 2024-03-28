3b9f50e46aa263aa69c2b391b2e5a5848531e732
codecShivam <codecshivam@gmail.com>
2024-03-28 13:29:15 +0530

    build: interactions component

---
+++ apps/web/app/marketplace/components/InteractionsList.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+
+export default function InteractionsList({ interactions }) {
+    return (
+        <>
+            {interactions.map((interaction, index) => (
+                <div key={index} className="flex md:flex-row md:space-y-auto space-y-3 flex-col justify-between w-full ">
+                    <div className="flex items-center gap-3">
+                        <div className="flex flex-col justify-start">
+                            <Image
+                                alt={interaction.name}
+                                loading="lazy"
+                                src={interaction.image}
+                                className="aspect-[1.08] sm:w-[70px] w-[50px] shrink-0 flex-1"
+                                width={70}
+                                height={100}
+                            />
+                        </div>
+                        <div className="flex flex-col">
+                            <div className="text-white sm:text-[24px] text-2xl font-medium leading-8 tracking-[1%]">{interaction.status}</div>
+                            <div className="text-sm sm:tracking-[1%] tracking-normal font-medium leading-8 text-white text-opacity-50">
+                                You interacted with {interaction.name}.
+                            </div>
+                        </div>
+                    </div>
+                    <button className="px-[3.5rem] py-1 my-auto text-base tracking-[1%] leading-8 font-medium transition-colors duration-100 hover:bg-neutral-800 rounded-[10px] bg-[#1C1C1C] text-zinc-400 max-md:px-5 sm:mr-12 mr-6">
+                        View More
+                    </button>
+                </div>
+            ))}
+        </>
+    );
+}
--- apps/web/app/marketplace/components/Transactions.tsx
+++ apps/web/app/marketplace/components/Transactions.tsx
@@ -1,34 +1,37 @@
-import Image from 'next/image';
-import sentImage from '../../../public/images/marketplace/sent.png';
-import receivedImage from '../../../public/images/marketplace/received.png';
-import transactions from '../data/transactionsData.json'
+'use client';
+
+import { useState } from 'react';
+import InteractionsList from './InteractionsList';
+import transactionsData from '../data/transactionsData.json';
+import TransactionsList from './TransactionsList';
+import interactionsData from '../data/interactionsData.json'
 
 export default function Transactions() {
+    const [activeButton, setActiveButton] = useState('Transactions');
+
+    const handleClick = (buttonName: string) => {
+        setActiveButton(buttonName);
+    };
+
     return (
-        <div className="flex flex-col xl:px-24 sm:px-12 px-6 w-full  xl:-mt-20 ">
-            <div className="flex sm:gap-5 gap-4 sm:ml-9  max-w-full sm:text-3xl text-xl font-medium tracking-wide leading-7 text-white whitespace-nowrap w-[408px] max-md:mt-10 max-md:ml-2.5">
-                <div className="justify-center p-2.5 rounded-xl bg-neutral-900">Transactions</div>
-                <div className="flex-auto my-auto">Interactions</div>
+        <div className="flex flex-col xl:px-24 sm:px-12 px-6 w-full xl:-mt-20 ">
+            <div className="flex sm:gap-5 gap-4 sm:ml-9 max-w-full sm:text-3xl text-xl font-medium tracking-wide leading-7 text-white whitespace-nowrap w-[408px] max-md:mt-10 max-md:ml-2.5">
+                <button
+                    className={`justify-center p-2.5 rounded-xl transition-colors duration-100 ${activeButton === 'Transactions' ? 'bg-neutral-900' : 'bg-transparent'} hover:bg-neutral-800`}
+                    onClick={() => handleClick('Transactions')}
+                >
+                    Transactions
+                </button>
+                <button
+                    className={`flex-auto p-2.5 rounded-xl my-auto transition-colors duration-100 ${activeButton === 'Interactions' ? 'bg-neutral-900' : 'bg-transparent'} hover:bg-neutral-800`}
+                    onClick={() => handleClick('Interactions')}
+                >
+                    Interactions
+                </button>
             </div>
             <div className="flex flex-col items-start sm:pl-12 pl-6 space-y-12 sm:py-14 py-7 sm:mx-2.5 mt-10 rounded-2xl bg-neutral-900">
-                {transactions.map((transaction, index) => (
-                    <div key={index} className="flex items-center gap-3">
-                        <div className="flex flex-col justify-start">
-                            <Image
-                                alt={transaction.type === 'Sent' ? 'sent icon' : 'received icon'}
-                                loading="lazy"
-                                src={transaction.type === 'Sent' ? sentImage : receivedImage}
-                                className="aspect-[1.08] sm:w-[70px] w-[50px]"
-                            />
-                        </div>
-                        <div className="flex flex-col">
-                            <div className="text-white sm:text-[24px] text-2xl font-medium leading-8 tracking-[1%]">{transaction.type}</div>
-                            <div className="text-sm sm:tracking-[1%] tracking-normal font-medium leading-8 text-white text-opacity-50">
-                                {transaction.type === 'Sent' ? `You sent ${transaction.amount} to ${transaction.recipient}` : `You received ${transaction.amount} from ${transaction.sender}`}
-                            </div>
-                        </div>
-                    </div>
-                ))}
+                {activeButton === 'Transactions' && <TransactionsList transactions={transactionsData} />}
+                {activeButton === 'Interactions' && <InteractionsList interactions={interactionsData} />}
             </div>
         </div>
     );
