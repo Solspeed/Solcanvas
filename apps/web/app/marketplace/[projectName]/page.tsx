2ea42c6962fc3f48b7cef0cb273b48a301f90177
codecShivam <codecshivam@gmail.com>
2024-03-29 20:49:36 +0530

    build: commits section

--- apps/web/app/marketplace/[projectName]/page.tsx
+++ apps/web/app/marketplace/[projectName]/page.tsx
@@ -276,7 +276,59 @@ export default function ProjectDetails({
                         </button>
                     )}
                 </div>
-               
+                <div className="flex flex-col sm:mt-64 mt-24 w-full">
+                    <div className="mx-2.5  sm:text-5xl mb-3 text-4xl tracking-tighter font-semibold sm:tracking-wider leading-8 text-white max-md:mt-10 max-md:max-w-full max-md:text-4xl">
+                        Commits
+                    </div>
+                    <div className="flex gap-5 self-center lg:px-10 sm:px-6 px-4 lg:py-11 sm:py-8 py-4 sm:mt-11 mt-6 w-full font-medium text-white rounded-2xl bg-neutral-900 max-w-[1345px] flex-wrap">
+                        <div className="flex-auto sm:text-2xl text-lg lg:text-3xl tracking-wide leading-8 max-md:max-w-full">
+                            8bpx5gh7sg....
+                        </div>
+                        <div className="flex items-center gap-2 xl:-mr-6">
+                            <div className="lg:text-2xl text-[#42FF00] self-baseline sm:text-xl text-base tracking-wide leading-8">
+                                Live
+                            </div>
+                            <div className="w-[18px] h-[18px] rounded-full bg-[#42FF00] inline-block "></div>
+                        </div>
+                    </div>
+                    <div className="flex gap-5 self-center lg:px-10 sm:px-6 px-4 lg:py-11 sm:py-8 py-4 sm:mt-11 mt-6 w-full font-medium text-white rounded-2xl bg-neutral-900 max-w-[1345px] flex-wrap">
+                        <div className="flex-auto sm:text-2xl text-lg lg:text-3xl tracking-wide leading-8 max-md:max-w-full">
+                            8bpx5gh7sg....
+                        </div>
+                        <div className="lg:text-2xl sm:text-xl text-base tracking-wide leading-8">
+                            Past
+                        </div>
+                    </div>
+                    <div className="flex gap-5 self-center lg:px-10 sm:px-6 px-4 lg:py-11 sm:py-8 py-4 sm:mt-11 mt-6 w-full font-medium text-white rounded-2xl bg-neutral-900 max-w-[1345px] flex-wrap">
+                        <div className="flex-auto sm:text-2xl text-lg lg:text-3xl tracking-wide leading-8 max-md:max-w-full">
+                            8bpx5gh7sg....
+                        </div>
+                        <div className="lg:text-2xl sm:text-xl text-base tracking-wide leading-8">
+                            Past
+                        </div>
+                    </div>
+                    <div className="flex gap-5 self-center lg:px-10 sm:px-6 px-4 lg:py-11 sm:py-8 py-4 sm:mt-11 mt-6 w-full font-medium text-white rounded-2xl bg-neutral-900 max-w-[1345px] flex-wrap">
+                        <div className="flex-auto sm:text-2xl text-lg lg:text-3xl tracking-wide leading-8 max-md:max-w-full">
+                            8bpx5gh7sg....
+                        </div>
+                        <div className="lg:text-2xl sm:text-xl text-base tracking-wide leading-8">
+                            Past
+                        </div>
+                    </div>
+                    <div className={`flex gap-5 ${showAll ? '' : 'hidden'} self-center lg:px-10 sm:px-6 px-4 lg:py-11 sm:py-8 py-4 sm:mt-11 mt-6 w-full font-medium text-white rounded-2xl bg-neutral-900 max-w-[1345px] flex-wrap`}>
+                        <div className="flex-auto sm:text-2xl text-lg lg:text-3xl tracking-wide leading-8 max-md:max-w-full">
+                            8bpx5gh7sg....
+                        </div>
+                        <div className="lg:text-2xl sm:text-xl text-base tracking-wide leading-8">
+                            Past
+                        </div>
+                    </div>
+                    {!showAll && (
+                        <button onClick={handleSeeAllClick} className="self-center sm:mt-12 mt-6 sm:text-3xl text-xl font-medium tracking-wide leading-8 text-zinc-400 max-md:mt-10">
+                            See all
+                        </button>
+                    )}
+                </div>
             </div>
         </div>
     )
