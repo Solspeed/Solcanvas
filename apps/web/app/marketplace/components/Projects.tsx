81d07a778bda751bb0cde988287a3edad308ae35
codecShivam <codecshivam@gmail.com>
2024-03-29 23:41:35 +0530

    fix: deployment issue

--- apps/web/app/marketplace/components/Projects.tsx
+++ apps/web/app/marketplace/components/Projects.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Card from "./Card";
 import projectsData from "../data/projectsData.json";
 
-// Define the type for your project data
 interface Project {
   imageSrc: string;
   iconSrc: string;
@@ -10,22 +9,20 @@ interface Project {
   description: string;
 }
 
+function splitIntoChunks<T>(array: T[], chunkSize: number): T[][] {
+  const chunks: T[][] = [];
+
+  for (let i = 0; i < array.length; i += chunkSize) {
+    const chunk: T[] = array.slice(i, i + chunkSize);
+    chunks.push(chunk);
+  }
+
+  return chunks;
+}
+
 export default function Projects(): JSX.Element {
   // Split the projectsData into chunks of two
-  const chunks: Project[][] = projectsData.reduce((resultArray: Project[][], item: Project, index: number) => {
-    const chunkIndex: number = Math.floor(index / 2);
-
-    // Ensure resultArray[chunkIndex] is not undefined
-    if (resultArray[chunkIndex] === undefined) {
-      // Use type assertion to assure TypeScript that resultArray[chunkIndex] is an array
-      resultArray[chunkIndex] = [] as Project[];
-    }
-
-    // Push the item to resultArray[chunkIndex]
-    resultArray[chunkIndex].push(item)
-
-    return resultArray;
-  }, []);
+  const chunks: Project[][] = splitIntoChunks(projectsData, 2);
 
   return (
     <div className="flex flex-col xl:gap-16 place-items-center space-y-[4.688rem] xl:px-24 sm:px-12 px-6">
@@ -33,14 +30,13 @@ export default function Projects(): JSX.Element {
         <div key={rowIndex} className="flex sm:gap-12 gap-[4.688rem] justify-between w-full sm:flex-nowrap flex-wrap">
           {chunk.map((project: Project, colIndex: number) => (
             <Card
-              key={rowIndex * 2 + colIndex} // Ensure each Card has a unique key
+              key={`${rowIndex}_${colIndex}`}
               imageSrc={project.imageSrc}
               iconSrc={project.iconSrc}
               title={project.title}
               description={project.description}
             />
           ))}
-          {/* Render empty cards if the chunk doesn't contain two items */}
           {chunk.length < 2 && (
             <div className="w-[calc(50%-4.688rem)]"></div>
           )}
