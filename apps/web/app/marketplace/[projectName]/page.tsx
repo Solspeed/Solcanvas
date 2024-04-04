94f458d71eeee4b2c22155ef1f03fa51b515a613
codecShivam <codecshivam@gmail.com>
2024-04-05 00:51:16 +0530

    fix the button and overflow issue

--- apps/web/app/marketplace/[projectName]/page.tsx
+++ apps/web/app/marketplace/[projectName]/page.tsx
@@ -1,11 +1,8 @@
 'use client'
+
 import CustomButton from "../../../components/button/CustomButton";
 import Link from "next/link";
 import Overview from "./components/Overview";
-import Team from "./components/Team";
-import Commits from "./components/Commits";
-import Updates from "./components/Updates";
-import Tweets from "./components/Tweets";
 import { useState, useEffect } from "react";
 import supabase from "../../../supabase";
 
@@ -64,18 +61,22 @@ export default function ProjectDetails({ params }: ProjectDetailsProps) {
     };
 
     return (
-        <div className="flex flex-col xl:px-14 sm:px-10 px-4 xl:py-16 sm:py-12 py-6 h-full bg-black">
+        <div className="flex flex-col xl:px-14 sm:px-10 px-4 xl:py-16 sm:py-12 py-6 h-full bg-black min-h-screen">
             <div className="flex flex-col w-full max-md:px-5 max-md:max-w-full">
                 <Link href="/marketplace" className="text-white text-opacity-60 text-lg font-semibold">
                     get back to marketplace
                 </Link>
                 <Overview projectsList={projectData.projectList} />
                 <div className="flex flex-col items-center px-6 w-full font-medium leading-8">
-                    <CustomButton
-                        text="Edit Project"
-                        property1="variant-2"
-                        className="flex sm:scale-[1] scale-[0.8] hover:cursor-pointer justify-center items-center text-center px-16 py-10 sm:mt-80 mt-36 max-w-full tracking-wider leading-8 bg-black rounded-2xl text-zinc-400 w-[972px]"
-                    />
+                    <Link href={`/marketplace/${params.projectName}/editform`}>
+                     
+                            <CustomButton
+                                text="Edit Project"
+                                property1="variant-2"
+                                className="flex sm:scale-[1] scale-[0.8] hover:cursor-pointer justify-center items-center text-center px-16 py-10 sm:mt-80 mt-36 max-w-full tracking-wider leading-8 bg-black rounded-2xl text-zinc-400 w-[972px]"
+                            />
+                       
+                    </Link>
                     <div className="sm:mt-80 mt-36 sm:text-xl text-lg text-center text-white text-opacity-60">
                         All right reserved @solcanvas
                     </div>
