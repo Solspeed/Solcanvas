1988a74e15ddbedced00b97287932b20c5aaec2c
codecShivam <codecshivam@gmail.com>
2024-04-05 20:53:22 +0530

    fetched the project

--- apps/web/app/marketplace/components/Card.tsx
+++ apps/web/app/marketplace/components/Card.tsx
@@ -7,27 +7,46 @@ interface CardProps {
   iconSrc: string;
   title: string;
   description: string;
+  url: string;
 }
 
-const Card: React.FC<CardProps> = ({ imageSrc, iconSrc, title, description }) => {
+const Card: React.FC<CardProps> = ({ imageSrc, iconSrc, title, description , url}) => {
+  // Function to extract limited words from description
+  const getLimitedDescription = (description: string, limit: number): string => {
+    const words = description.split(" ");
+    if (words.length > limit) {
+      return words.slice(0, limit).join(" ") + "...";
+    }
+    return description;
+  };
+
+  const limitedDescription = getLimitedDescription(description, 8);
+
   return (
-    <div className="w-[45.875rem] h-[19.063rem] bg-[#151515] rounded-[15px]">
+    <a href={`/marketplace/${url}`} className="w-[45.875rem] h-[19.063rem] bg-[#151515] rounded-[15px]">
       <Image
         src={imageSrc}
         alt={title}
         width={734}
         height={152.5}
-        className="w-full shrink-0 object-center object-cover h-[50%] rounded-t-[15px]" />
+        className="w-full shrink-0 object-center object-cover h-[50%] rounded-t-[15px]"
+      />
       <div className="flex items-center sm:ml-6 ml-2 gap-4 h-[50%]">
-        <Image src={iconSrc} alt={`${title} icon`} width={90} height={90} className="shrink-0 flex aspect-square" />
+        <Image
+          src={iconSrc}
+          alt={`${title} icon`}
+          width={90}
+          height={90}
+          className="shrink-0 flex aspect-square"
+        />
         <div className="flex flex-col justify-center gap-2">
           <div className="text-white font-semibold text-[22px]">{title}</div>
-          <div className="text-[#B2B2B2] xl:tracking-normal md:tracking-tight tracking-tighter text-[15px]  ">
-            {description}
+          <div className="text-[#B2B2B2] xl:tracking-normal md:tracking-tight tracking-tighter text-[15px]">
+            {limitedDescription}
           </div>
         </div>
       </div>
-    </div>
+    </a>
   );
 };
 
