0434f58d6106ca684aace9d846702648f5c0eabd
codecShivam <codecshivam@gmail.com>
2024-04-18 17:52:13 +0530

    footer changed

--- apps/web/components/layout/Footer.tsx
+++ apps/web/components/layout/Footer.tsx
@@ -2,79 +2,70 @@ import Image from "next/image";
 import twitter from "../../public/Twitter.svg"
 import github from "../../public/Github.svg"
 
-export default function Footer() {
+interface FooterLinkProps {
+    title: string;
+    links: string[];
+}
+
+const FooterLink: React.FC<FooterLinkProps> = ({ title, links }) => {
     return (
-        <footer className="self-stretch xl:px-24 sm:px-16 px-6 bg-black flex flex-col items-start justify-between box-border max-w-full gap-[1.25rem] z-[4] text-left text-[1rem] text-mediumorchid font-nunito mq450:pt-[3.875rem] mq450:px-[1.25rem] mq450:pb-[3.063rem] mq450:box-border mq725:pl-[3.75rem] mq725:pr-[3.75rem] mq725:box-border mq1000:flex-wrap">
-            <div className="w-full flex flex-col gap-12">
-                <div className="w-full bg grid grid-cols-3 gap-[4.75rem] max-w-full mq450:gap-[2.375rem] mq725:flex-wrap">
-                    <div className="flex flex-col items-start justify-start gap-[1.188rem]  ">
-                        <a href="#" className="relative tracking-[0.05em] inline-block min-w-[3.75rem] z-[1] font-bold  text-plum-100 ">
-                            Navigate
-                        </a>
-                        <a href="#" className="relative tracking-[0.05em] inline-block min-w-[3.75rem] z-[1]  text-plum-100">
-                            Explore
-                        </a>
-                        <a href="#" className="relative tracking-[0.05em] inline-block min-w-[2.25rem] z-[1]  text-plum-100">
-                            Earn
-                        </a>
-                        <a href="#" className="relative tracking-[0.05em] inline-block min-w-[2.813rem] z-[1]  text-plum-100">
-                            Learn
-                        </a>
+        <div className="flex flex-col flex-1">
+            <a href={`/${title.toLowerCase()}`} className="font-bold text-purple-400">{title}</a>
+            {links.map((link, index) => (
+                <a href={`/${link.toLowerCase()}`} key={index} className={`mt-${index === 0 ? 6 : 4}`}>
+                    {link}
+                </a>
+            ))}
+        </div>
+    );
+};
 
-                    </div>
-                    <div className="flex flex-col items-start justify-start gap-[1.188rem]  ">
-                        <a href="#" className="relative tracking-[0.05em] inline-block min-w-[2.125rem] z-[1] font-bold  text-plum-100 ">
-                            Help
-                        </a>
-                        <a href="#" className="relative tracking-[0.05em] inline-block min-w-[2.125rem] z-[1]  text-plum-100">
-                            FAQ
-                        </a>
-                        <a href="#" className="relative tracking-[0.05em] inline-block min-w-[3.813rem] z-[1]  text-plum-100">
-                            Contact
-                        </a>
-                        <a href="#" className="relative tracking-[0.05em] inline-block min-w-[3.063rem] z-[1]  text-plum-100">
-                            About
-                        </a>
-                    </div>
-                    <div className="flex flex-col items-start justify-start gap-[1.188rem]  ">
-                        <a href="#" className="relative tracking-[0.05em] inline-block min-w-[2.125rem] z-[1] font-bold  text-plum-100 ">
-                            Legal
-                        </a>
-                        <a href="#" className="relative tracking-[0.05em] inline-block min-w-[2.125rem] z-[1]  text-plum-100">
-                            Privacy
-                        </a>
-                        <a href="#" className="relative tracking-[0.05em] inline-block min-w-[3.813rem] z-[1]  text-plum-100">
-                            Terms
-                        </a>
-                        <a href="#" className="relative tracking-[0.05em] inline-block min-w-[3.063rem] z-[1]  text-plum-100">
-                            Cookies
-                        </a>
-                    </div>
+const footerData = [
+    {
+        title: "Navigate",
+        links: ["Explore", "Earn", "Learn"],
+    },
+    {
+        title: "Help",
+        links: ["FAQ", "Contact", "About"],
+    },
+    {
+        title: "Legal",
+        links: ["Privacy", "Terms", "Cookies"],
+    },
+];
+
+function Footer() {
+    return (
+        <footer className="flex justify-center items-center sm:px-16 px-4 sm:py-20 py-6 w-full bg-black max-md:px-5 max-md:max-w-full">
+            <div className="flex flex-col mt-5 w-full max-w-[1200px] max-md:max-w-full">
+                <div className="flex gap-5 text-base tracking-wider text-purple-300 whitespace-nowrap max-md:flex-wrap max-md:pr-5 max-md:max-w-full">
+                    {footerData.map((data, index) => (
+                        <FooterLink key={index} title={data.title} links={data.links} />
+                    ))}
                 </div>
-                <div className="flex justify-between sm:gap-0 gap-4  sm:mr-28">
-                    <div className="relative tracking-[0.05em] font-archivo   z-[1]  text-plum-100">
-                        © 2024 Solcanvas. All Rights R eserved.
+                <div className="flex gap-5 sm:mt-14 mt-6 w-full max-md:flex-wrap max-md:mt-10 max-md:max-w-full">
+                    <div className="flex-auto text-base tracking-wider text-purple-300">
+                        © 2024 Solcanvas. All Rights Reserved.
                     </div>
-
-                    <div className=" flex flex-row items-start justify-end">
-                        <div className="w-[4.5rem] flex flex-row items-start justify-between gap-[1.25rem]">
-                            <Image
-                                className="h-[1.25rem] w-[1.25rem] relative overflow-hidden shrink-0 min-h-[1.25rem] z-[1]  text-plum-100"
-                                loading="lazy"
-                                alt=""
-                                src={twitter}
-                            />
-                            <Image
-                                className="h-[1.231rem] w-[1.25rem] relative min-h-[1.25rem] z-[1]  text-plum-100"
-                                loading="lazy"
-                                alt=""
-                                src={github}
-                            />
-                        </div>
+                    <div className="flex gap-5 justify-between self-start">
+                        <Image
+                            loading="lazy"
+                            src={twitter}
+                            alt="Social media icon"
+                            className="shrink-0 w-5 aspect-square"
+                        />
+                        <Image
+                            loading="lazy"
+                            src={github}
+                            alt="Social media icon"
+                            className="shrink-0 w-5 aspect-square fill-purple-600"
+                        />
                     </div>
                 </div>
             </div>
         </footer>
     );
-};
+}
 
+export default Footer;
