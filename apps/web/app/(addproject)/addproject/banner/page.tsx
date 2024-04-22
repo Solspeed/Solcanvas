d51baea826e07e8098e545050e8e953d424d43f9
codecShivam <codecshivam@gmail.com>
2024-04-23 00:20:28 +0530

    update landing page

--- apps/web/app/(addproject)/addproject/banner/page.tsx
+++ apps/web/app/(addproject)/addproject/banner/page.tsx
@@ -2,13 +2,13 @@
 import { useState } from "react";
 import { ChangeEvent } from "react";
 import required from "../../../../public/images/required.png";
-import next from "../../../public/images/next.png";
+import next from "../../../../public/images/next.png";
 import Image from "next/image";
-
+import upload from "../../../../public/images/upload.png";
 export default function ProjectBanner() {
 
     return (
-        <form className="flex flex-col self- sm:mt-24 font-nunito max-w-full mt-10 ">
+        <form className="flex flex-col  self- sm:mt-24 font-nunito max-w-full mt-10 ">
             <div className="flex gap-2.5 self-start">
                 <div className="shrink-0 rounded-2xl bg-zinc-400 h-[17px] w-[38px]" />
                 <div className="shrink-0 w-14 bg-[#954AD2] rounded-2xl h-[17px]" />
@@ -20,7 +20,77 @@ export default function ProjectBanner() {
             <div className="mt-5 text-2xl font-nunito font-medium tracking-wide leading-8 text-white text-opacity-50 max-w-full">
                 Show world your product design skill of banner and logo.
             </div>
-
+            <div className="flex gap-5  justify-between  w-full text-2xl tracking-wide leading-8 whitespace-nowrap sm:flex-none flex-wrap sm:mt-20 mt-10 max-w-full">
+                <div className="flex flex-1 self-start  text-white text-opacity-80">
+                    <div>Logo</div>
+                    <Image
+                        alt=""
+                        width={100}
+                        height={100}
+                        loading="lazy"
+                        src={required}
+                        className="shrink-0 self-start w-3.5 aspect-square"
+                    />
+                </div>
+            </div>
+            <div className="flex gap-5 py-6 pr-3.5 pl-7 mt-8 w-fit bg-purple-300 rounded-xl max-md:flex-wrap max-md:pl-5">
+                <img
+                    loading="lazy"
+                    src={upload.src}
+                    className="shrink-0 my-auto  mr-4  aspect-square stroke-[2px] stroke-black w-[25px]"
+                />
+                <div className="flex flex-col  grow shrink-0 basis-0 w-fit">
+                    <div className="text-[30px] font-medium tracking-normal leading-8 text-blue-600">
+                        <span className="text-black ">Drop, Paste here or</span>{" "}
+                        <span className="text-blue-600 ">Browse</span>
+                    </div>
+                    <div className="mt-4 text-lg tracking-normal leading-8 text-black text-opacity-50">
+                        Recommended size: 250*250 | JPG, PNG, GIF. Max sie: 50MB
+                    </div>
+                </div>
+            </div>
+            <div className="flex gap-5  w-full text-2xl tracking-wide leading-8 whitespace-nowrap flex-wrap mt-10 max-w-full">
+                <div className="flex flex-1  self-start text-white text-opacity-80">
+                    <div>Banner</div>
+                    <Image
+                        alt=""
+                        width={100}
+                        height={100}
+                        loading="lazy"
+                        src={required}
+                        className="shrink-0 self-start w-3.5 aspect-square"
+                    />
+                </div>
+            </div>
+            <div className="flex gap-5 py-6 pr-3.5 pl-7 mt-8 w-fit bg-purple-300 rounded-xl max-md:flex-wrap max-md:pl-5">
+                <img
+                    loading="lazy"
+                    src={upload.src}
+                    className="shrink-0 my-auto  mr-4  aspect-square stroke-[2px] stroke-black w-[25px]"
+                />
+                <div className="flex flex-col grow shrink-0 basis-0 w-fit">
+                    <div className="text-[30px] font-medium tracking-normal leading-8 text-blue-600">
+                        <span className="text-black ">Drop, Paste here or</span>{" "}
+                        <span className="text-blue-600 ">Browse</span>
+                    </div>
+                    <div className="mt-4 text-lg tracking-normal leading-8 text-black text-opacity-50">
+                        Recommended size: 1500*500 | JPG, PNG, GIF. Max sie: 50MB
+                    </div>
+                </div>
+            </div>
+            <div className="flex gap-5 justify-end  w-full text-2xl font-medium tracking-wide leading-7 whitespace-nowrap flex-wrap sm:mt-20 mt-10 max-w-full">
+                <a href="/addproject/banner" className="flex gap-5  font-nunito justify-between  items-center sm:-mr-12 sm:px-7 py-4 text-white text-opacity-80 bg-[#954AD2] rounded-3xl px-5">
+                    <div>Finish</div>
+                    <Image
+                        alt=""
+                        width={100}
+                        height={100}
+                        loading="lazy"
+                        src={next}
+                        className="shrink-0 w-4  aspect-[0.76] stroke-[2px] stroke-white"
+                    />
+                </a>
+            </div>
         </form>
     );
 }
--- apps/web/components/layout/Navbar.tsx
+++ apps/web/components/layout/Navbar.tsx
@@ -12,14 +12,16 @@ export default function Navbar() {
     const [showMenu, setShowMenu] = useState(false);
     const [animateHeader, setAnimateHeader] = useState(false);
     const [connected, setConnected] = useState(false);
-    // const { publicKey } = useWallet();
+    const { publicKey } = useWallet();
     const connection = useConnection();
     const wallet = useWallet();
+
     useEffect(() => {
         if (wallet.connected) {
             setConnected(true);
         }
     }, [connection, wallet]);
+
     useEffect(() => {
         setAnimateHeader(true);
     }, []);
@@ -39,27 +41,83 @@ export default function Navbar() {
         config: { tension: 250, friction: 20 },
     });
 
-    const fadeInAnimation = useSpring({
-        opacity: showMenu ? 1 : 0,
-        config: { tension: 250, friction: 20 },
-        delay: showMenu ? 5 : 0,
-    });
-
     const toggleMenu = () => {
         setShowMenu(!showMenu);
     };
 
+    const LargeScreenNavbar = () => (
+        <div className="hidden sm:flex items-baseline justify-center lg:justify-between gap-[1.94rem]">
+            <a
+                href="/#"
+                className="relative tracking-[0.01em] leading-[1.88rem] capitalize font-semibold z-[1] transition duration-300 ease-in-out hover:text-purple-500 hover:border-purple-500 hover:shadow-md"
+            >
+                Explore
+            </a>
+            {wallet.connected ? (
+                <a
+                    href="/#"
+                    className="rounded-full bg-black flex items-center justify-center px-[1.5rem] whitespace-nowrap z-[1] border-[1px] border-solid border-gray-400 transition duration-300 ease-in-out hover:text-purple-500 hover:border-purple-500 hover:shadow-md"
+                >
+                    <div className="relative tracking-[0.01em] leading-[1.88rem] capitalize font-semibold z-[1]">
+                        Add project
+                    </div>
+                </a>
+            ) : (
+                <a
+                    href="/#"
+                    className="rounded-full bg-black flex items-center justify-center px-[1.5rem] whitespace-nowrap z-[1] border-[1px] border-solid border-gray-400 transition duration-300 ease-in-out hover:text-purple-500 hover:border-purple-500 hover:shadow-md"
+                >
+                    <div className="relative tracking-[0.01em] leading-[1.88rem] capitalize font-semibold z-[1]">
+                        Add project
+                    </div>
+                </a>
+            )}
+        </div>
+    );
+
+    const SmallScreenNavbar = () => (
+        <div className="sm:hidden fixed -top-12 -left-6 h-screen   bg-black  z-50 ">
+            <div className="flex flex-col items-center justify-center pr-12 pl-8 pt-7">
+              
+                <a
+                    href="/#"
+                    className="text-white py-2 px-4 m-4 capitalize font-semibold border-b border-gray-600 transition duration-300 ease-in-out hover:text-purple-500 hover:border-purple-500 hover:shadow-md"
+                >
+                    Explore
+                </a>
+                {wallet.connected ? (
+                    <a
+                        href="/#"
+                        className="text-white py-2 px-4 m-4 capitalize font-semibold border-b border-gray-600 transition duration-300 ease-in-out hover:text-purple-500 hover:border-purple-500 hover:shadow-md"
+                    >
+                        Add project
+                    </a>
+                ) : (
+                    <a
+                        href="/#"
+                        className="text-white py-2 px-4 m-4 capitalize font-semibold border-b border-gray-600 transition duration-300 ease-in-out hover:text-purple-500 hover:border-purple-500 hover:shadow-md"
+                    >
+                        Add project
+                    </a>
+                )}
+                {/* <div className="text-white py-2 px-4 m-4 capitalize font-semibold border-b border-gray-600 transition duration-300 ease-in-out hover:text-purple-500 hover:border-purple-500 hover:shadow-md">
+                    <WalletMultiButton />
+                </div> */}
+            </div>
+        </div>
+    );
+
     return (
         <animated.header
-            className="flex-1 rounded-full bg-black shadow-[0px_4px_50px_rgba(134,_122,_210,_0.2)] flex flex-row items-end justify-between pt-[1.31rem] pb-[1.44rem] pr-[1.44rem] pl-[1.75rem] box-border gap-[1.25rem] max-w-full text-left sm:text-[1rem] text-sm text-white relative"
+            className="flex-1 rounded-full  bg-black shadow-[0px_4px_50px_rgba(134,_122,_210,_0.2)] flex flex-row items-end justify-between pt-[1.31rem] pb-[1.44rem] pr-[1.44rem] pl-[1.75rem] box-border gap-[1.25rem] max-w-full text-left sm:text-[1rem] text-sm text-white relative"
             style={headerAnimation}
         >
             <div className="h-[4.88rem] w-[64.44rem] rounded-3xl bg-black shadow-[0px 4px 50px rgba(134, 122, 210, 0.2)] hidden max-w-full" />
             <div className="flex items-center justify-start gap-[0.44rem] font-nunito">
                 <Image src={logo} alt="solcanvas logo" className="sm:w-8 w-6 " />
                 <b className="relative tracking-[0.03em] leading-[1.875rem] uppercase inline-block ">
-              SolCanvas
-            </b>
+                    SolCanvas
+                </b>
             </div>
             <div className="flex items-center justify-center lg:justify-between gap-[1.94rem]">
                 <div className="sm:hidden">
@@ -67,81 +125,9 @@ export default function Navbar() {
                         {showMenu ? <FaTimes size={24} /> : <FaBars size={24} />}
                     </button>
                 </div>
-                {showMenu && (
-                    <animated.div
-                        className="sm:hidden fixed inset-0 bg-black bg-opacity-80 rounded-full z-50 "
-                        style={slideAnimation}
-                    >
-                        <animated.div style={fadeInAnimation}>
-                            <div className="flex items-center justify-end p-4">
-                                <button onClick={toggleMenu} className="text-white">
-                                    <FaTimes size={32} />
-                                </button>
-                            </div>
-                            <div className="flex flex-col items-center justify-center h-6">
-                                <animated.a
-                                    href="/marketplace"
-                                    className="text-white py-2 px-4 m-4 capitalize font-semibold border-b border-gray-600 transition duration-300 ease-in-out hover:text-purple-500 hover:border-purple-500 hover:shadow-md"
-                                    style={fadeInAnimation}
-                                >
-                                    Explore
-                                </animated.a>
-                                {wallet.connected ? (
-                                    <animated.a
-                                        href="/addproject"
-                                        className="text-white py-2 px-4 m-4 capitalize font-semibold border-b border-gray-600 transition duration-300 ease-in-out hover:text-purple-500 hover:border-purple-500 hover:shadow-md"
-                                        style={fadeInAnimation}
-                                    >
-                                        Add project
-                                    </animated.a>
-                                ) : (
-                                    <animated.a
-                                        href="/connectwallet"
-                                        className="text-white py-2 px-4 m-4 capitalize font-semibold border-b border-gray-600 transition duration-300 ease-in-out hover:text-purple-500 hover:border-purple-500 hover:shadow-md"
-                                        style={fadeInAnimation}
-                                    >
-                                        Add project
-                                    </animated.a>
-                                )}
-                                <animated.div
-                                    className="text-white py-2 px-4 m-4 capitalize font-semibold border-b border-gray-600 transition duration-300 ease-in-out hover:text-purple-500 hover:border-purple-500 hover:shadow-md"
-                                    style={fadeInAnimation}
-                                >
-                                    <WalletMultiButton />
-                                </animated.div>
-                            </div>
-                        </animated.div>
-                    </animated.div>
-                )}
-                <div className="hidden sm:flex items-baseline justify-center lg:justify-between gap-[1.94rem]">
-                    <a
-                        href="/marketplace"
-                        className="relative tracking-[0.01em] leading-[1.88rem] capitalize font-semibold z-[1] transition duration-300 ease-in-out hover:text-purple-500 hover:border-purple-500 hover:shadow-md"
-                    >
-                        Explore
-                    </a>
-                    {wallet.connected ? (
-                        <a
-                            href="/addproject"
-                            className="rounded-full bg-black flex items-center justify-center px-[1.5rem] whitespace-nowrap z-[1] border-[1px] border-solid border-gray-400 transition duration-300 ease-in-out hover:text-purple-500 hover:border-purple-500 hover:shadow-md"
-                        >
-                            <div className="relative tracking-[0.01em] leading-[1.88rem] capitalize font-semibold z-[1]">
-                                Add project
-                            </div>
-                        </a>
-                    ) : (
-                        <a
-                            href="/connectwallet"
-                            className="rounded-full bg-black flex items-center justify-center px-[1.5rem] whitespace-nowrap z-[1] border-[1px] border-solid border-gray-400 transition duration-300 ease-in-out hover:text-purple-500 hover:border-purple-500 hover:shadow-md"
-                        >
-                            <div className="relative tracking-[0.01em] leading-[1.88rem] capitalize font-semibold z-[1]">
-                                Add project
-                            </div>
-                        </a>
-                    )}
-
-                </div>
+                <LargeScreenNavbar />
             </div>
+                {showMenu && <SmallScreenNavbar />}
         </animated.header>
     );
 }
