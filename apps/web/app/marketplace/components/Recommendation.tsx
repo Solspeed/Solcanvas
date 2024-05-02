4dd9d22f116a57ad44292b85cc9321e0c00ad258
codecShivam <codecshivam@gmail.com>
2024-05-02 09:34:14 +0530

    update marketplace page with new design

--- apps/web/app/marketplace/components/Recommendation.tsx
+++ apps/web/app/marketplace/components/Recommendation.tsx
@@ -9,11 +9,11 @@ import Link from "next/link"
 
 export default function Recommendation() {
     return (
-        <div className="flex xl:px-24  sm:px-12 px-6   overflow-hidden flex-col justify-center font-medium text-white capitalize min-h-[380px]">
+        <div className="flex xl:px-24 font-nunito  sm:px-12 px-6   overflow-hidden flex-col justify-center font-medium text-white capitalize min-h-[380px]">
             <div className="bg-cover bg-center px- bg-no-repeat rounded-3xl" style={{ backgroundImage: `url(${bgImage.src})` }}>
                 <div className="flex relative flex-col sm:px-10 px-4 sm:py-12 py-6 w-full rounded-2xl bg-black bg-opacity-30  max-md:px-5 max-md:max-w-full">
-                    <div className="text-xl tracking-wide leading-8 max-md:max-w-full">
-                        RECOMMENDEDATION
+                    <div className="text-xl font-silkscreen tracking-wide leading-8 max-md:max-w-full">
+                        RECOMMENDED
                     </div>
                     <div className="shrink-0 mt-6 h-px border border-solid bg-zinc-400 border-zinc-400 max-md:max-w-full" />
                     <div className="flex sm:justify-normal sm:flex-nowrap flex-wrap justify-evenly sm:gap-5 gap-2 self-center mt-10 w-full text-base tracking-normal leading-8 whitespace-nowrap max-w-[1419px] max-md:flex-wrap max-md:max-w-full">
