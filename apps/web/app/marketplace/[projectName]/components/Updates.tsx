81a5d6452048415b2e3f1f0653f12d221c3e8368
codecShivam <codecshivam@gmail.com>
2024-05-02 10:13:02 +0530

    making changes to project page

--- apps/web/app/marketplace/[projectName]/components/Updates.tsx
+++ apps/web/app/marketplace/[projectName]/components/Updates.tsx
@@ -11,10 +11,10 @@ export default function Updates() {
     return (
 
         <div className="flex flex-col sm:mt-64 mt-24 w-full">
-            <div className="mx-2.5  sm:text-5xl mb-3 text-4xl tracking-tighter font-semibold sm:tracking-wider leading-8 text-white max-md:mt-10 max-md:max-w-full max-md:text-4xl">
+            <div className="mx-2.5 font-silkscreen text-center text-[#954AD2]  sm:text-[60px] mb-3 text-4xl tracking-tighter font-medium sm:tracking-wider leading-8  ">
                 Product Updates
             </div>
-            <div className="flex scale-[0.8] gap-5 self-center lg:px-10 sm:px-6 px-4 lg:py-11 sm:py-8 py-4 sm:mt-11 mt-6 w-full font-medium text-white rounded-2xl bg-neutral-900 max-w-[1390px] flex-wrap">
+            <div className="flex scale-[0.8] gap-5 self-center lg:px-10 sm:px-6 px-4 lg:py-11 sm:py-8 py-4 sm:mt-11 mt-6 w-full font-medium  rounded-2xl bg-neutral-900 max-w-[1390px] flex-wrap">
                 <div className="flex-auto sm:text-2xl text-lg lg:text-3xl tracking-wide leading-8 max-md:max-w-full">
                     Started their first round of funding
                 </div>
@@ -22,7 +22,7 @@ export default function Updates() {
                     25 Jan, 2024
                 </div>
             </div>
-            <div className={`flex scale-[0.8] gap-5 ${showAllUpdates ? '' : 'blur-[3px]'} self-center lg:px-10 sm:px-6 px-4 lg:py-11 sm:py-8 py-4 sm:mt-11 mt-6 w-full font-medium text-white rounded-2xl bg-neutral-900 max-w-[1350px] flex-wrap`}>
+            <div className={`flex scale-[0.8] gap-5 ${showAllUpdates ? '' : 'blur-[3px]'} self-center lg:px-10 sm:px-6 px-4 lg:py-11 sm:py-8 py-4 sm:mt-11 mt-6 w-full font-medium  rounded-2xl bg-neutral-900 max-w-[1350px] flex-wrap`}>
                 <div className="flex-auto sm:text-2xl text-lg lg:text-3xl tracking-wide leading-8 max-md:max-w-full">
                     Started their first round of funding
                 </div>
@@ -30,7 +30,7 @@ export default function Updates() {
                     25 Jan, 2024
                 </div>
             </div>
-            <div className={`flex scale-[0.8] gap-5 ${showAllUpdates ? '' : 'hidden'} self-center lg:px-10 sm:px-6 px-4 lg:py-11 sm:py-8 py-4 sm:mt-11 mt-6 w-full font-medium text-white rounded-2xl bg-neutral-900 max-w-[1350px] flex-wrap`}>
+            <div className={`flex scale-[0.8] gap-5 ${showAllUpdates ? '' : 'hidden'} self-center lg:px-10 sm:px-6 px-4 lg:py-11 sm:py-8 py-4 sm:mt-11 mt-6 w-full font-medium  rounded-2xl bg-neutral-900 max-w-[1350px] flex-wrap`}>
                 <div className="flex-auto sm:text-2xl text-lg lg:text-3xl tracking-wide leading-8 max-md:max-w-full">
                     Started their first round of funding
                 </div>
