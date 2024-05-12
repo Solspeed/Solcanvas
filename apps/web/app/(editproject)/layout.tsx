b1fb2b1d4250a34e87bae35663d7bf7fe811fa65
Shivam Yadav <codecshivam@gmail.com>
2024-05-12 18:43:26 +0000

    building editproject forms

---
+++ apps/web/app/(editproject)/layout.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import logo from "../../public/logo.png"
+
+export default function ProjectLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}): JSX.Element {
+  return (
+    <div className="overflow-x-hidden min-h-screen bg-black flex py-6 justify-center px-6">
+      <div className="flex flex-col sm:ml-24 mx-auto">
+        {children}
+      </div>
+    </div>
+  )
+}
